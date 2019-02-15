b4194f7e782e600af83c8a0dcb0dd11e563c7372
anthony <Anthony@BC12-Anthony.enclave.com>
2019-02-15 17:07:50 +0700

    have error, need fix

--- server.js
+++ server.js
@@ -1,3 +1,76 @@
+let data = [{
+    "id": 1,
+    "first_name": "Mayor",
+    "last_name": "Orrow",
+    "email": "morrow0@behance.net",
+    "gender": "Male",
+    "city": "Hechuan"
+}, {
+    "id": 2,
+    "first_name": "Suzette",
+    "last_name": "Iacobo",
+    "email": "siacobo1@ustream.tv",
+    "gender": "Female",
+    "city": "Pyin Oo Lwin"
+}, {
+    "id": 3,
+    "first_name": "Betti",
+    "last_name": "Moncey",
+    "email": "bmoncey2@zdnet.com",
+    "gender": "Female",
+    "city": "Tambakbaya"
+}, {
+    "id": 4,
+    "first_name": "Selina",
+    "last_name": "Sarson",
+    "email": "ssarson3@illinois.edu",
+    "gender": "Female",
+    "city": "Jepat Kidul"
+}, {
+    "id": 5,
+    "first_name": "Alyse",
+    "last_name": "Scryne",
+    "email": "ascryne4@wiley.com",
+    "gender": "Female",
+    "city": "Sakaidechō"
+}, {
+    "id": 6,
+    "first_name": "Angela",
+    "last_name": "Hustler",
+    "email": "ahustler5@house.gov",
+    "gender": "Female",
+    "city": "Znamenskoye"
+}, {
+    "id": 7,
+    "first_name": "Port",
+    "last_name": "Broadberry",
+    "email": "pbroadberry6@privacy.gov.au",
+    "gender": "Male",
+    "city": "Grängesberg"
+}, {
+    "id": 8,
+    "first_name": "Fonsie",
+    "last_name": "Oleszczak",
+    "email": "foleszczak7@stumbleupon.com",
+    "gender": "Male",
+    "city": "Ji’an"
+}, {
+    "id": 9,
+    "first_name": "Hesther",
+    "last_name": "Planke",
+    "email": "hplanke8@mit.edu",
+    "gender": "Female",
+    "city": "Azenha"
+}, {
+    "id": 10,
+    "first_name": "Lexis",
+    "last_name": "Hazelby",
+    "email": "lhazelby9@skyrock.com",
+    "gender": "Female",
+    "city": "Taoling"
+}];
+
+
 var express = require("express");
 var app = express();
 
@@ -11,7 +84,7 @@ var app = express();
 app.use(cors());
 
 app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+    res.json(data);
 });
 
 app.listen(3001, () => {
--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+
+let data =  function readData()
+{
   fetch('http://localhost:3001/url')
   .then(function(response) {
     console.log(response.json());
+    return response;
   })
   .then(function(myJson) {
     console.log(JSON.stringify(myJson));
   });
+  return response;
+}
+
+class App extends Component {
+  render() {
+
+    let content = data.map((student,index) => 
+      <tr key = {index}>
+        <td>{student.id}</td>
+        <td>{student.first_name}</td>
+        <td>{student.last_name}</td>
+      </tr>
+    );  
     return (
-      <p></p>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Frist name</th>
+            <th>Last name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {content}
+        </tbody>
+      </table>
     );
   }
 }
